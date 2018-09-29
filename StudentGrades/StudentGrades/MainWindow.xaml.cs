8f5e40b2073ffb8ae00f217667dc8ae08e41f5fa
Jake Stanger <stangbang27@gmail.com>
2018-09-29 16:30:39 -0600

    grade arrays working

--- StudentGrades/StudentGrades/MainWindow.xaml.cs
+++ StudentGrades/StudentGrades/MainWindow.xaml.cs
@@ -20,8 +20,11 @@ namespace StudentGrades
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string[] Students;
-        public string[,] StudentGrades;
+        public string[] AryStudents;
+        public int[,] AryStudentGrades;
+        //public Array<string, int> AryStudentGrades;
+        public int iStudentCount;
+        public int iAssignmentCount;
 
         public MainWindow()
         {
@@ -34,16 +37,46 @@ namespace StudentGrades
             if ( ValidateCounts() )
             {
                 //init the student array and the grades array - student 1, student 2 grades 0.
+                SetCountValues(tbxNumOfStudents.Text, tbxNumOfAssignments.Text);
+                InitArrays(iStudentCount, iAssignmentCount);
             }
 
         }
 
+        public void SetCountValues(string stuCount, string asntCount)
+        {
+            Int32.TryParse(stuCount, out iStudentCount);
+            Int32.TryParse(asntCount, out iAssignmentCount);
+
+        }
+
         //helper methods
+        public void InitArrays(int iStuCount, int iAsntCount)
+        {
+            AryStudents = new string[iStuCount];
+            AryStudentGrades = new int[iStuCount, iAsntCount];
+
+            for (int i = 0; i < iStuCount; i++)
+            {
+                string sCount = "Student #" + (i+1).ToString();
+                AryStudents[i] = sCount;
+                Console.WriteLine("here is: " + AryStudents[i]);
+                for (int j = 0; j < iAsntCount; j++)
+                {
+                    AryStudentGrades[i, j] = 0;
+                    Console.Write("grades: " + AryStudentGrades[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
         public bool ValidateCounts()
         {
+            //TODO: validate
             //if good don't print error and return true
             // else print error message and return false
-            return false;
+            return true;
         }
     }
 }
